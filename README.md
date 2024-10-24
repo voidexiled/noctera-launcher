87ce5e29bf3216ca2b132a64a9b8f39e91fd67e2
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-24 02:30:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Canary Launcher Update
+* C# WPF
+* .NET 6.0
+
+### Information
+
+* ✅ Launcher like Tibia Global
+* ✅ Download client
+* ✅ Auto check update
+* ✅ Update client
+* ✅ Run the client
+
+You must configure the "launcher_config.json" url in MainWindow.cs and SplashScreen.cs
+
+In launcher_config.json you need to make necessary settings to use the launcher. (Read the explanation of how to use each configuration)
---
+++ src/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CanaryLauncherUpdate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			App app = new App();
+			app.InitializeComponent();
+			app.Run();
+		}
+	}
+}
